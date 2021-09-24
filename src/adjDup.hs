c685d9f2434ec54a3154611d4c99ffc28c73770e
HIRTIH KUMAR C V <61385197+cv-hirtihk@users.noreply.github.com>
2021-09-24 19:21:44 +0530

    Explanation

--- src/adjDup.hs
+++ src/adjDup.hs
@@ -1,3 +1,4 @@
+{-Removes adjacent duplicates from a given list. i.e. if the same element occurs n times contiguously, we retain only one copy.-}
 adjDup:: [Int] -> [Int]
 adjDup [x] = [x]
 adjDup (x:y:xs)
